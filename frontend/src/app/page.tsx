8f0a4d483daf413e67adc22b90c90851736691b7
Juan Saldana <saldanaj97@gmail.com>
2024-08-02 16:21:01 -0500

    chore: Update main container class in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center">
+      <main className="container flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             WannabeHired.ai
