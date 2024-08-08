5a903ce6bb7b0fd6b97612b1c108308335f16d91
Juan Saldana <saldanaj97@gmail.com>
2024-08-08 10:46:09 -0500

    chore: Update main container class in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,7 @@
 export default async function Home() {
   return (
-    <main className="container flex min-h-screen flex-col items-center justify-center">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">WannabeHired.ai</h1>
-        <div className="flex flex-col items-center gap-2">
-          <div className="flex flex-col items-center justify-center gap-4"></div>
-        </div>
-      </div>
+    <main className="container flex min-h-screen min-w-full flex-col items-center justify-center">
+      <span className="text-5xl font-extrabold sm:text-[5rem] tracking-tight text-center">WannabeHired.ai</span>
     </main>
   )
 }
