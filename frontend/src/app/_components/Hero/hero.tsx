514317e09f1e25a819397448fd5e5152f826f52a
Juan Saldana <saldanaj97@gmail.com>
2024-08-09 12:43:20 -0500

    refactor: Update Hero component and add TypingEffect

--- frontend/src/app/_components/Hero/hero.tsx
+++ frontend/src/app/_components/Hero/hero.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@nextui-org/react';
 import { FaArrowDown } from 'react-icons/fa';
+import { TypingEffect } from './TypingEffect';
 
 export default function Hero() {
   return (
     <div className="container mx-auto flex max-w-3xl flex-1 flex-col items-center justify-center text-center">
       <h1 className="mb-4 text-4xl font-semibold tracking-tight sm:text-6xl">
-        Elevate Your Job Search Experience
+        <TypingEffect word="Elevate Your Job Search Experience" />
       </h1>
       <p className="mb-8 w-full text-lg font-normal tracking-tight sm:text-xl">
         Our AI-powered assistant tailors job recommendations, crafts compelling
